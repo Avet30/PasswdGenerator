a7593fe82d4c540768dde8026e3b1b299c440bf5
Avet <Avet@DESKTOP-B0OJ1SS>
2022-06-01 23:13:00 +0200

    Ajoutez des fichiers projet.

---
+++ PasswdGenerator/Outils.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswdGenerator
+{
+    public static class Outils
+    {
+        public static int AskNumberPositiveNotNull(string question)
+        {
+            return AskNumberBetween(question, 1, int.MaxValue);
+        }
+        public static int AskNumberBetween(string question, int min, int max)
+        {
+            while (true)
+            {
+                int number = AskNumber(question);
+
+                if ((number >= min) && (number <= max))
+                {
+                    return number;
+                }
+                Console.WriteLine($"Wrong Choice please enter a number between {min} and {max} !");
+            }
+        }
+        public static int AskNumber(string question)
+        {
+            while (true)
+            {
+                Console.Write(question);
+                string answer = Console.ReadLine();
+                try
+                {
+                    int answerInt = int.Parse(answer);
+                    return answerInt;
+                }
+                catch
+                {
+                    Console.WriteLine("Please enter a number");
+                }
+            }
+        }
+    }
+}
---
+++ PasswdGenerator/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using PasswdGenerator;
+
+namespace PasswdGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int NbOfPasswords = 10;
+            int lengthOfPasswd = Outils.AskNumberPositiveNotNull("Length of Password : ");
+            Console.WriteLine();
+
+            int ChoiceOfAlphabet = Outils.AskNumberBetween("Would like a password with :\n" +
+                "1 - Only lowercase characters\n" +
+                "2 - lowercase and uppercase characters\n" +
+                "3 - characters and numbers\n" +
+                "4 - characters, numbers and special characters\n" +
+                "Your choice : ", 1, 4);
+
+
+            string lowercase = "abcdefghijklmnopqrstuvxyz";
+            string uppercase = lowercase.ToUpper();
+            string numbers = "123456789";
+            string specialCaracters = "!@#$%";
+            string alphabet = lowercase + uppercase + numbers + specialCaracters;
+            string password = "";
+            
+            Random random = new Random();
+
+            if (ChoiceOfAlphabet == 1)
+            {
+                alphabet = lowercase;
+            }
+            else if (ChoiceOfAlphabet == 2)
+            {
+                alphabet = lowercase + uppercase;
+            }
+            else if (ChoiceOfAlphabet == 3)
+            {
+                alphabet = lowercase + uppercase + numbers;
+            }
+            else
+            {
+                alphabet = lowercase + uppercase + numbers + specialCaracters;
+            }
+
+            int lenghtOfAlphabet = alphabet.Length;
+
+            for(int j = 0; j < NbOfPasswords; j++)
+            {
+                password = "";
+                for (int i = 0; i < lengthOfPasswd; i++)
+                {
+                    int index = random.Next(0, lenghtOfAlphabet);
+                    password += alphabet[index];
+                  
+                }
+                Console.WriteLine($"Password : {password}");
+            }           
+        }
+    }
+}
